c907ca13d77e79d7a72b0dc5ddba75c1e9fba1e1
aluguf2811 <28leva11list.ru>
2017-11-07 19:38:25 +0400

    I added methods add and exists

--- Store/src/com/company/Main.java
+++ Store/src/com/company/Main.java
@@ -4,17 +4,14 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        ProductsDao dao = new ProductsDaoTxtImplementation();
-//        File file = new File("products.txt");
-//        Reader reader = new FileReader(file);
-//        BufferedReader br = new BufferedReader(reader);
-//        String str = br.readLine();
-//        String str1 = br.readLine();
-//        String str2 = br.readLine();
-//        System.out.println(str);
-//        System.out.println(str1);
-//        System.out.println(str2);
-        dao.remove("rom");
+    public static void main(String[] args) throws IOException, ProductNotFoundException {
+        File file = new File("products.csv");
+        ProductsDao dao = new ProductsDaoCsvImplementation();
+        System.out.println(dao.getprice("яблоки"));
+
+        //Product product = new Product("хлеб", 40, "Хлебзавод1");
+        //dao.add(product);
+
+        System.out.println(dao.exists("печенье"));
     }
 }
--- Store/src/com/company/ProductsDao.java
+++ Store/src/com/company/ProductsDao.java
@@ -5,20 +5,12 @@ import java.io.IOException;
 public interface ProductsDao {
     boolean exists(String product) throws IOException;
 
-    void add(String product) throws IOException;
+    void add(Product product) throws IOException;
 
-    /**
-     * Удаляет конкретный продукт
-     * @param product название продукта, который надо удалить
-     */
+    int getprice(String productName) throws IOException;
+
     void remove(String product) throws IOException;
 
-    /**
-     * Заменяет один продукт на другой
-     * @param product1 продукт, который будет заменен
-     * @param product2 продукт, который заменит первый продукт
-     */
     void replace(String product1, String product2) throws IOException;
 
-    int testsize() throws IOException;
 }
